86e6ac20982b9e7724fb0e35b9e021a030ee78e2
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2024-05-31 11:28:42 +0700

    Jobsheet 12 : E - Membuat Symlink

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -20,8 +20,12 @@ class FileUploadController extends Controller
         ]);
         $extfile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-'.time().".".$extfile;
-        $path = $request->berkas->storeAs('uploads', $namaFile);
+        $path = $request->berkas->storeAs('public', $namaFile);
+
+        $pathBaru = asset('storage/'.$namaFile);
         echo "proses upload berhasil, data disimpan pada: $path";
+        echo "<br>";
+        echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
         // echo $request->berkas->getClientOriginalName() . "lolos validasi";
     }
 }
